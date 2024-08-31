4037efc46153ea91b9b4168e7eeaa64cad0587cd
omololevy <cotechlevy@gmail.com>
2024-08-31 21:05:38 +0300

    Create a decorator function for the timing

--- decorators.py
+++ decorators.py
@@ -14,3 +14,15 @@ def log_activity(func):
         return result
     return wrapper
 
+def timing(func):
+    """
+    Decorator for time the execution of a function.
+    """
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"{func.__name__} executed in {execution_time:.4f} seconds")
+        return result
+    return wrapper
